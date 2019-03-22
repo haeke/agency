b1dda1256ee6ac7cd9aec7657f808bbe7898b90e
haeke <riverae2@student.wpunj.edu>
2019-03-22 15:53:36 -0400

    changed the services component and added a team component

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -2,49 +2,26 @@ import React from "react";
 
 import Article from "../Article/Article";
 
+import "./Services.css";
+
 const Services = () => {
   return (
     <section className="servicesContainer">
-      <article className="services">
-        <h1 className="servicesTitle">OUR SERVICES</h1>
+      <article className="servicesHeaderContainer">
+        <h1 className="servicesHeader">OUR SERVICES</h1>
       </article>
       <Article
         articleClass="services"
-        imgSrc="/images/news.jpeg"
+        imgSrc="/images/services.jpg"
         imgName="News"
-        articleHeaderClass="homeArticleHeader"
-        articleHeader="news."
-        articleParagraphClass="homeArticleParagraph"
+        articleHeaderClass="serviceArticleHeader"
+        articleHeader="HOW DO WE WORK?"
+        articleParagraphClass="serviceArticleParagraph"
         articleParagraph="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio
             dolores molestiae corrupti facere. Ad amet est quam magnam maxime
             eum rerum debitis maiores ratione? Fugit consequuntur asperiores
             iste officiis reiciendis?"
       />
-      <article className="services">
-        <div className="leftContent">
-          <img src="/images/news.jpeg" alt="news" />
-        </div>
-        <div className="rightContent">
-          <div>
-            <h1 className="servicesTitle">HOW DO WE WORK?</h1>
-            <hr />
-            <p className="servicesDescription">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error
-              magnam sed possimus dolor illum quo assumenda. Eum, animi eligendi
-              quod in omnis sapiente porro. Fuga sequi quae aliquam natus ex.
-            </p>
-            <p className="servicesDescription">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-              atque assumenda quod? Dolorem excepturi vitae libero
-              necessitatibus dolore quisquam laboriosam?Lorem ipsum dolor, sit
-              amet consectetur adipisicing elit. Quibusdam, quae laudantium?
-              Nihil ut ab aspernatur eligendi omnis natus doloribus enim
-              voluptatum accusantium in.
-            </p>
-          </div>
-        </div>
-      </article>
-      <article className="services" />
     </section>
   );
 };
---
+++ src/components/Team/Team.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Team.css";
+
+const Team = () => {
+  return (
+    <article className="teamContainer">
+      <div className="teamHeaderContainer">
+        <h1 className="teamHeader">OUR TEAM</h1>
+      </div>
+    </article>
+  );
+};
+
+export default Team;
